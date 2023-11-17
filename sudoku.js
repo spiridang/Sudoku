49501c77b43f45d2833827b91c79689647196b03
alexeyShinkarevv <alexeyshinkarevv@gmail.com>
2023-11-17 11:00:49 +0300

    solve

--- sudoku.js
+++ sudoku.js
@@ -4,17 +4,19 @@ const { EOL } = require('os');
 
 function read() {
   let sudoku = text.trim().split(`${EOL}`);
-
-  return sudoku.map((el) => el.split(''));
+// console.log(sudoku)
+  return sudoku.map((el) => el.split('')); 
 }
-
+read()
 function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+  const puzzle = read()[1];
+  const puzzleArray = [];
+  for (let i = 9; i < puzzle.length + 1; i += 9) {
+    puzzleArray.push(puzzle.slice(i - 9, i));
+  }console.log(puzzleArray)
 }
 
+solve()
 function isSolved() {
   /**
    * Принимает игровое поле в том формате, в котором его вернули из функции solve.
